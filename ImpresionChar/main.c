a8551808e3625a498dc70fe85d44d5850a616893
Frandevep <frandevep@gmail.com>
2025-01-05 14:36:16 -0300

    Add files via upload

---
+++ ImpresionChar/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+
+int main()
+{
+    int num=10; //dar valor numerico a una variable, num es el vector de la variable int
+    float numdecimal=2.33; //permite dar valor a una variable decimal
+    char letra='F';//permite ingresar un caracter
+
+    printf("%d",num); // imprimir esa variable con valor numerico
+    printf("\n");//genera salto de linea para visualizar mejor la impresion
+    printf("%f",numdecimal); //imprime variable decimal
+    printf("\n%c",letra); //imprime un UNICO caracter
+
+
+    return 0;
+}
